8066f77007d10ae2238c46e9a9dcec6e6a68c962
Syeda Umme Hani <36325175+hani29@users.noreply.github.com>
2022-02-23 16:46:14 +0530

    changes in index

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import configureStore from './store/configureStore';
-import { fetchTodos } from './store/actions/todos';
-import AppRouter from './router/AppRouter';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import configureStore from "./store/configureStore";
+import AppRouter from "./router/AppRouter";
 
 const store = configureStore();
 
-const renderApp = async () => {
-	await store.dispatch(fetchTodos());
-	const jsx = (
-		<Provider store={store}>
-			<AppRouter />
-		</Provider>
-	);
-	ReactDOM.render(jsx, document.getElementById('root'));
-}
+const Todo = () => {
+  return (
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>
+  );
+};
 
-
-ReactDOM.render(<p>loading...</p>, document.getElementById('root'));
-
-renderApp();
+ReactDOM.render(<Todo />, document.getElementById("root"));
