117b7d91ce8dd24d5c54396631c253c68ae12f45
Syeda Umme Hani <36325175+hani29@users.noreply.github.com>
2022-02-23 16:54:41 +0530

    Update AppRouter.js

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router , Route, Switch, Redirect } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
 import App from '../containers/App';
 import TodoListDetails from '../components/TodoList/TodoListDetails';
@@ -9,7 +9,7 @@ export const history = createHistory();
 const AppRouter = () => (
 	<Router history={history}>
 		<Switch>
-			<Route exact={true} path="/" component={App} />
+			<Route exact path="/" component={App} />
 			<Route path="/edit/:id" component={TodoListDetails} />
 			<Redirect path="*" to="/"/>
 		</Switch>
